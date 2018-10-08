cd6fb18d4a1bc7da3f0b92ead36cd548af20e564
PlMaria <43480535+PlMaria@users.noreply.github.com>
2018-10-08 19:33:17 +0300

    Update task re-regular expressions -1.py

--- task re-regular expressions -1.py
+++ task re-regular expressions -1.py
@@ -1,21 +1,6 @@
 import urllib.request
 import re
 
-def get_url_count(url):
-    with urllib.request.urlopen(url) as webpage:
-        for line in webpage:
-            r=re.compile(r'http(s)?://')
-            t=0
-            try:
-                r.search('http(s)?://').group()
-                t=t+1
-            except:
-                continue
-
-    return t
-
-#print(get_url_count("https://dfedorov.spb.ru/python3/"))
-
 def get_url_count1(url):
     t=0
     with urllib.request.urlopen(url) as webpage:
