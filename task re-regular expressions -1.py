3a47999d1e1be83fa8828ed9f65e89212a2c9f57
PlMaria <43480535+PlMaria@users.noreply.github.com>
2018-10-08 19:52:50 +0300

    Update task re-regular expressions -1.py

--- task re-regular expressions -1.py
+++ task re-regular expressions -1.py
@@ -1,7 +1,7 @@
 import urllib.request
 import re
 
-def get_url_count1(url):
+def get_url_count(url):
     t=0
     with urllib.request.urlopen(url) as webpage:
         for line in webpage:
@@ -12,8 +12,28 @@ def get_url_count1(url):
            t=t+s
     return t
 
-print(get_url_count1("https://dfedorov.spb.ru/python3/"))
+#print(get_url_count("https://dfedorov.spb.ru/python3/"))
 
 '''
 19
 '''
+
+def get_url_count1(url):
+    t=0
+    try:
+        with urllib.request.urlopen(url) as webpage:
+            text=webpage.read().decode('utf-8')
+           
+        r=re.findall('http(s)?://',text)
+        t=len(r)
+        return t
+    
+    except Exception as e:
+        return e
+
+#print(get_url_count1("https://dfedorov.spb.ru/python3/"))
+
+if __name__=='__main__':
+    url="https://dfedorov.spb.ru/python3/"
+    print(get_url_count(url))
+    print(get_url_count1(url))
